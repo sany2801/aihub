5eddc8bdfa1fae7cc8386bbefcf720c57bb458ee
Гранцевич Александр <37817378+sany2801@users.noreply.github.com>
2023-10-23 01:19:26 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-const express = require('express');
-const axios = require('axios');
-const cron = require('node-cron');
-const OpenAI = require('openai');
+// const express = require('express');
+// const axios = require('axios');
+// const cron = require('node-cron');
+// const OpenAI = require('openai');
 
 const app = express();
 const port = process.env.PORT || 3000;
